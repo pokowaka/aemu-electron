cad4a2bda7fb690607c5aa8e38f7e9b30cb04776
Erwin Jansen <jansene@google.com>
2021-02-02 11:54:35 -0800

    Add support for display size.
    You can now specify the window size. If the window size is specified you
    will not be able to resize the window.
    
    The size can be set by passing the width & height parameters as follows:
    
    -size [width]x[height]
    
    Where both widht and height are numbers.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { app, BrowserWindow, Menu } from "electron";
+import { app, BrowserWindow } from "electron";
 import debug from "electron-debug";
 import unhandled from "electron-unhandled";
-import * as path from "path";
 import is from "electron-util";
+import * as path from "path";
 
 app.on("ready", () => {
 	console.log("App is ready");
@@ -16,19 +16,30 @@ app.setAppUserModelId("com.pokowaka@gmail.com.aemu-electron");
 // Prevent window from being garbage collected
 let mainWindow: BrowserWindow | undefined;
 
-
 const createMainWindow = async () => {
+	const argv: string[] = process.argv;
+	let resize = true;
+	let width = 1080;
+	let height = 1920;
+
+	if (argv.includes("-size")) {
+		const dimensions = argv[argv.indexOf("-size") + 1];
+		const extract = /(\d+)x(\d+)/.exec(dimensions);
+		width = parseInt(extract![1]);
+		height = parseInt(extract![2]);
+		resize = false;
+	}
+
 	const win = new BrowserWindow({
 		title: "Android Emulator View",
 		show: false,
-		width: 1080 / 3,
-		height: 1920 / 3,
+		width: width,
+		height: height,
+		resizable: resize,
 		webPreferences: {
 			nodeIntegration: true
-	}});
-
-
-
+		}
+	});
 
 	win.on("ready-to-show", () => {
 		win.show();
@@ -61,7 +72,6 @@ app.on("second-instance", () => {
 
 app.on("window-all-closed", () => {
 	if (!is.is.macos) {
-
 		app.quit();
 	}
 });
